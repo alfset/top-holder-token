1c53aae97fefce7a34f5e2c4c52a0e8dc5861f82
Comunity-Node <42708476+alfset@users.noreply.github.com>
2023-08-08 05:54:03 +0700

    Update collect.js

--- collect.js
+++ collect.js
@@ -2,7 +2,7 @@ import Web3 from 'web3';
 
 const web3 = new Web3(new Web3.providers.HttpProvider('https://evm-rpc.planq.network'))
 
-let blockNum = 0
+let blockNum = 4322268
 
 let run = async () => {
     let addresses = {}
@@ -16,18 +16,18 @@ let run = async () => {
         console.log('block', blck, 'transactions', block.transactions.length)
         for (let i = 0; i < block.transactions.length; i++) {
             let tx = await web3.eth.getTransaction(block.transactions[i])
-            if (parseInt(tx.value) > 0) {
+            if (parseInt(tx.to) > 0) {
                 addresses[tx.to] = true
             }
         }
     }
 
     let positiveAddresses = []
-    for (address in addresses) {
+    for (addresses in addresses) {
         try {
-            let balance = await web3.eth.getBalance(address)
+            let balance = await web3.eth.getBalance(addresses)
             if (balance > 0) {
-                positiveAddresses.push(address)
+                positiveAddresses.push(addresses)
             }
         } catch (err) {
             console.log(err)
